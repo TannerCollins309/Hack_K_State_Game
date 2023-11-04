0ed0cdd537fc23a28365a7488084270afd8c5d09
Tanner Collins <72166757+TannerCollins309@users.noreply.github.com>
2023-11-04 15:41:39 -0500

    Trying to find location of colliding objects

---
+++ Assets/levels/lvl1/Node2D.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _physics_process(delta):
+	move_local_x(-1)
--- Assets/levels/lvl1/testTower.gd
+++ Assets/levels/lvl1/testTower.gd
@@ -4,6 +4,13 @@ func _physics_process(delta):
 	turn()
 	
 func turn():
-	# var enemy_position = ?
-	# get_node("testTower").look_at(enemy_position)
-	pass
+	#if get_node("testTower").is_colliding():
+		# var collider = get_node("testTowerRange").get_collider()
+		#var enemy_position = get_node("testTower").get_collision_point()
+		#get_node("testTower").look_at(enemy_position)
+		# print(collider)
+	var area = get_node("testTowerRange").get
+	print(area)
+
+func _on_area_2d_area_entered(area):
+	print("entered") # Replace with function body.
