633bc9c4a826ebcb40c91e156ca75654c0bdbb6d
lindnergunk <lindnergunk@lopers.unk.edu>
2023-11-04 15:37:38 -0500

    Functioning main menu transition to level

---
+++ play_scene.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var zombie_scene: PackedScene
+@export var level_scene: PackedScene
+
+var health = 100;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_hud_start_round():
+	pass # Replace with function body.
