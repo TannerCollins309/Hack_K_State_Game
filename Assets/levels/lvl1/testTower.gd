314fb06907d9ed5e65e3e5b46ab0bfa5902b6af6
Tanner Collins <72166757+TannerCollins309@users.noreply.github.com>
2023-11-04 23:50:49 -0500

    Tower now follows enemies

--- Assets/levels/lvl1/testTower.gd
+++ Assets/levels/lvl1/testTower.gd
@@ -2,31 +2,45 @@ extends Node2D
 
 var targetsInRange = []
 var currentTarget
+var location
 
 func _physics_process(delta):
 	turn()
 	
 func turn():
-	currentTarget = getTarget()
-	# print(currentTarget)
+	if targetsInRange:
+		currentTarget = getTarget()
+	if currentTarget:
+		location = getLocation(currentTarget)
+	print(location)
+	print(typeof(location))
+	if location:
+		get_node('TowerSprite').look_at(location)
 
 func getTarget():
-	#var farthest
-	#var farthestProgress = 0
-	# var progress
-	var node
+	var farthest
+	var farthestProgress = 0
+	var progress
 	for i in targetsInRange:
-		var progress = i.get_parent().get_progress_ratio()
-	return null
+		progress = i.get_parent().get_progress_ratio()
+		if progress > farthestProgress:
+			farthestProgress = progress
+			farthest = i
+	return farthest
+
+func getLocation(target):
+	var location = target.get_parent().global_position
+	return location
 
 func attack(target):
+	
 	pass
 
 func _on_test_tower_range_body_entered(body):
-	targetsInRange = get_node("testTowerRange").get_overlapping_bodies()
+	targetsInRange = get_node("TowerRange").get_overlapping_bodies()
 	print("entered")
 	print(targetsInRange)
 
 
 func _on_test_tower_range_body_exited(body):
-	targetsInRange = get_node("testTowerRange").get_overlapping_bodies()
+	targetsInRange = get_node("TowerRange").get_overlapping_bodies()
