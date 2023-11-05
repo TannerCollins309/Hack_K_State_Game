447427b6526bc047dae479af34facb2ce96f9ead
Tanner Collins <72166757+TannerCollins309@users.noreply.github.com>
2023-11-04 20:56:30 -0500

    Tower now detects zombies

--- Assets/levels/lvl1/testTower.gd
+++ Assets/levels/lvl1/testTower.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
+var targetsInRange = []
+
 func _physics_process(delta):
 	turn()
 	
 func turn():
-	#if get_node("testTower").is_colliding():
-		# var collider = get_node("testTowerRange").get_collider()
 		#var enemy_position = get_node("testTower").get_collision_point()
 		#get_node("testTower").look_at(enemy_position)
 		# print(collider)
 	var area = get_node("testTowerRange").get
-	print(area)
+	# print(area)
+	#var area = get_node("testTowerRange").get
+	#print(area)
+	pass
 
 func _on_area_2d_area_entered(area):
-	print("entered") # Replace with function body.
+	pass
+
+func _on_test_tower_range_body_entered(body):
+	targetsInRange = get_node("testTowerRange").get_overlapping_bodies()
+	print("entered")
+	print(targetsInRange)
+
+
+func _on_test_tower_range_body_exited(body):
+	targetsInRange = get_node("testTowerRange").get_overlapping_bodies()
